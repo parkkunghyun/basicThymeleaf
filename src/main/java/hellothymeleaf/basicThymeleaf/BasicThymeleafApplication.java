a214bd4fb61974fdd831ef9b2a20b41619d8f1fc
baggyeonghyeon <rudgus4620@gmail.com>
2023-04-04 08:09:26 +0900

    thymeleaf- practice

---
+++ src/main/java/hellothymeleaf/basicThymeleaf/BasicThymeleafApplication.java
@@ -0,0 +1,13 @@
+package hellothymeleaf.basicThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hellothymeleaf/basicThymeleaf/BasicThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package hellothymeleaf.basicThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
